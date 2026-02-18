e4e34d588af54d62d8eb1e0594d51ad101bd33f6
Mhayk Whandson <mhayk@kaumediagroup.com>
2026-02-18 15:48:47 +0000

    feat: styled the page and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import { prisma } from '@/lib/prisma';
 
 export default function Home() {
   return (
-    <div>
-      <h1>MK</h1>
-    </div>
+    <section className="flex min-h-full">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-4">Select one prompt</h1>
+          <p className="text-gray-400">
+            Choose one of the prompts from the beside list to view and edit it.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
