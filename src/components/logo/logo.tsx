c1f7b0c821e0b9665bb14b6119c88b87a9e6f7f4
Mhayk Whandson <mhayk@kaumediagroup.com>
2026-03-02 16:48:42 +0000

    feat: create logo component
    Signed-off-by: Mhayk Whandson <mhayk@kaumediagroup.com>

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,14 @@
+import { MessagesSquare } from 'lucide-react';
+import Link from 'next/link';
+
+export const Logo = () => {
+  return (
+    <Link
+      href="/"
+      className="flex items-center gap-2 hover:text-accent-600 rounded-lg transition-colors"
+    >
+      <MessagesSquare />
+      <span className="text-lg font-semibold">PROMPTS</span>
+    </Link>
+  );
+};
